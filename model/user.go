3c4ad0c94c6877fbe11def632302172d577e9545
Terugit <nishino-teruki@g.ecc.u-tokyo.ac.jp>
2022-12-15 23:51:23 +0900

    truncated unused types

--- model/user.go
+++ model/user.go
@@ -6,20 +6,6 @@ type User struct {
 	Point int    `json:"point"`
 }
 
-type UserAdd struct {
-	Name string `json:"name"`
-}
-
-type userId struct {
-	Id string `json:"id"`
-}
-
-type UserPoint struct {
-	Id    string `json:"id"`
-	Name  string `json:"name"`
-	Point int    `json:"point"`
-}
-
 type UserRank struct {
 	Id    string `json:"id"`
 	Name  string `json:"name"`
